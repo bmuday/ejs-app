5192c56c773b05b782afb409da18b85bf2083482
baboulass <bmuday@live.fr>
2022-08-18 18:50:37 +0200

    fix mongo connection

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 mongoose
   .connect(process.env.DB_URI, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() => console.log("Connected to MongoDB!"))
   .catch((err) => "Database connection has failed!");
